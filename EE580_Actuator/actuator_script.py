eeb6f88b749198bbba3c0436c5745547be541674
Aisling Lee <aislinglee@gmail.com>
2022-08-17 01:15:29 +0100

    Adding Files from Actuator RPI which subscirbe to broker topics and control GPIO pin setting for LEDs

---
+++ EE580_Actuator/actuator_script.py
@@ -0,0 +1,76 @@
+import RPi.GPIO as GPIO
+import time
+import paho.mqtt.client as paho
+from paho import mqtt
+import re
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(6,GPIO.OUT)  # Red
+GPIO.setup(13,GPIO.OUT) # Orange
+GPIO.setup(19,GPIO.OUT) # Yellow
+GPIO.setup(26,GPIO.OUT) # Blue
+
+# setting callbacks for different events to see if it works, print the message etc.
+def on_connect(client, userdata, flags, rc, properties=None):
+    print("CONNACK received with code %s." % rc)
+
+# with this callback you can see if your publish was successful
+def on_publish(client, userdata, mid, properties=None):
+    print("mid: " + str(mid))
+
+# print which topic was subscribed to
+def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+# print message, useful for checking if it was successful
+def on_message(client, userdata, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    raw_payload = str(msg.payload)
+    # remove non alphanuemeric characters
+    trigger = int(re.sub('[^0-9]','', raw_payload))
+    print (trigger)
+    if(trigger == 1):
+     print("Turn ON LED")
+     # set GPIO pins to HIGH
+     GPIO.output(6,GPIO.HIGH)
+     GPIO.output(13,GPIO.HIGH)
+     GPIO.output(19,GPIO.HIGH)
+     GPIO.output(26,GPIO.HIGH)
+    if(trigger == 0 ):
+     print("Turn Off LED")
+     # set GPIO pins to Low
+     GPIO.output(6,GPIO.LOW)
+     GPIO.output(13,GPIO.LOW)
+     GPIO.output(19,GPIO.LOW)
+     GPIO.output(26,GPIO.LOW)
+
+# using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
+# userdata is user defined data of any type, updated by user_data_set()
+# client_id is the given name of the client
+client = paho.Client(client_id="", userdata=None, protocol=paho.MQTTv5)
+client.on_connect = on_connect
+
+# enable TLS for secure connection
+client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
+# set username and password
+client.username_pw_set("SN20216371", "EE580_alee")
+# connect to HiveMQ Cloud on port 8883 (default for MQTT)
+client.connect("3a7064825d8d4ce68f4d17cdf59b41e1.s1.eu.hivemq.cloud", 8883)
+
+# setting callbacks, use separate functions like above for better visibility
+client.on_subscribe = on_subscribe
+client.on_message = on_message
+client.on_publish = on_publish
+
+		
+# subscribe to all topics of encyclopedia by using the wildcard "#"
+client.subscribe("actuator_topic/act1", qos=1)
+
+# a single publish, this can also be done in loops, etc.
+client.publish("device_topic/device", payload="Actuator Online", qos=1)
+
+# loop_forever for simplicity, here you need to stop the loop manually
+# you can also use loop_start and loop_stop
+client.loop_forever()

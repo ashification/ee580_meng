c3079c1a62dec804d40e0cd231a9e3acfcc022b3
Aisling Lee <aislinglee@gmail.com>
2022-08-18 16:02:40 +0100

    Pushing test 2 - adapted decision logic code for probability of intent to use door

--- decision_logic.py
+++ decision_logic.py
@@ -16,44 +16,49 @@ from skfuzzy import control as ctrl
 import time
 from PIL import Image
 
-# Code adapted from https://pythonhosted.org/scikit-fuzzy/auto_examples/plot_tipping_problem_newapi.html
+# Code adapted from https://pythonhosted.org/scikit-fuzzy/auto_examples/plot_intention_problem_newapi.html
 
 # New Antecedent/Consequent objects hold universe variables and membership
 # functions
-quality = ctrl.Antecedent(np.arange(0, 11, 1), 'quality')
-service = ctrl.Antecedent(np.arange(0, 11, 1), 'service')
-tip = ctrl.Consequent(np.arange(0, 26, 1), 'tip')
+speed = ctrl.Antecedent(np.arange(0, 71, 1), 'speed')
+distance = ctrl.Antecedent(np.arange(0, 1001, 10), 'distance')
+intent = ctrl.Consequent(np.arange(0, 11, 1), 'intent')
 
 # Auto-membership function population is possible with .automf(3, 5, or 7)
-quality.automf(3)
-service.automf(3)
+speed.automf(3)
+distance.automf(3)
 
 # Custom membership functions can be built interactively with a familiar,
 # Pythonic API
-tip['low'] = fuzz.trimf(tip.universe, [0, 0, 13])
-tip['medium'] = fuzz.trimf(tip.universe, [0, 13, 25])
-tip['high'] = fuzz.trimf(tip.universe, [13, 25, 25])
+intent['low'] = fuzz.trimf(intent.universe, [0, 0, 5])
+intent['medium'] = fuzz.trimf(intent.universe, [0, 5, 10])
+intent['high'] = fuzz.trimf(intent.universe, [5, 10, 10])
 
-quality['average'].view()
-service.view()
-tip.view()
+speed['average'].view()
+distance.view()
+intent.view()
 
-rule1 = ctrl.Rule(quality['poor'] | service['poor'], tip['low'])
-rule2 = ctrl.Rule(service['average'], tip['medium'])
-rule3 = ctrl.Rule(service['good'] | quality['good'], tip['high'])
+rule1 = ctrl.Rule(speed['poor'] | distance['poor'], intent['low'])
+rule2 = ctrl.Rule(distance['average'] & speed['good'], intent['medium'])
+rule3 = ctrl.Rule(distance['good'] & speed['good'], intent['high'])
 
 rule1.view()
 
-tipping_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
-tipping = ctrl.ControlSystemSimulation(tipping_ctrl)
+intention_ctrl = ctrl.ControlSystem([rule1, rule2, rule3])
+intention = ctrl.ControlSystemSimulation(intention_ctrl)
 
 # Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
 # Note: if you like passing many inputs all at once, use .inputs(dict_of_data)
-tipping.input['quality'] = 6.5
-tipping.input['service'] = 9.8
+intention.input['speed'] = 61
+intention.input['distance'] = 434
 
+'''
+speed =  61 office  740  boxrm  702  bedrm  434
+speed =  60 office  744  boxrm  704  bedrm  440
+speed =  41 office  757  boxrm  715  bedrm  454
+'''
 # Crunch the numbers
-tipping.compute()
+intention.compute()
 
-print(tipping.output['tip'])
-tip.view(sim=tipping)
+print(intention.output['intent'])
+intent.view(sim=intention)

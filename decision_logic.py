8bf187367ae305d563d3b40ac42a96952d7ad483
Aisling Lee <aislinglee@gmail.com>
2022-08-18 18:38:43 +0100

    Pushing decision logic with rules refined through emprical methods before integration with broker system
    pushing db also to include data generate  during testing

--- decision_logic.py
+++ decision_logic.py
@@ -11,8 +11,10 @@
 
 
 import numpy as np
+import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
+from datetime import *
 import time
 from PIL import Image
 
@@ -21,7 +23,7 @@ from PIL import Image
 # New Antecedent/Consequent objects hold universe variables and membership
 # functions
 speed = ctrl.Antecedent(np.arange(0, 71, 1), 'speed')
-distance = ctrl.Antecedent(np.arange(0, 1001, 10), 'distance')
+distance = ctrl.Antecedent(np.arange(0, 800, 10), 'distance')
 intent = ctrl.Consequent(np.arange(0, 11, 1), 'intent')
 
 # Auto-membership function population is possible with .automf(3, 5, or 7)
@@ -31,7 +33,7 @@ distance.automf(3)
 # Custom membership functions can be built interactively with a familiar,
 # Pythonic API
 intent['low'] = fuzz.trimf(intent.universe, [0, 0, 5])
-intent['medium'] = fuzz.trimf(intent.universe, [0, 5, 10])
+intent['medium'] = fuzz.trimf(intent.universe, [2, 5, 8])
 intent['high'] = fuzz.trimf(intent.universe, [5, 10, 10])
 
 speed['average'].view()
@@ -39,8 +41,14 @@ distance.view()
 intent.view()
 
 rule1 = ctrl.Rule(speed['poor'] | distance['poor'], intent['low'])
-rule2 = ctrl.Rule(distance['average'] & speed['good'], intent['medium'])
-rule3 = ctrl.Rule(distance['good'] & speed['good'], intent['high'])
+rule2 = ctrl.Rule(speed['poor'] | distance['average'], intent['low'])
+rule3 = ctrl.Rule(speed['poor'] | distance['good'], intent['low'])
+
+rule4 = ctrl.Rule(distance['average'] & speed['average'], intent['medium'])
+rule5 = ctrl.Rule(distance['average'] & speed['good'], intent['medium'])
+
+rule6 = ctrl.Rule(distance['good'] & speed['average'], intent['high'])
+rule7 = ctrl.Rule(distance['good'] & speed['good'], intent['high'])
 
 rule1.view()
 
@@ -49,13 +57,17 @@ intention = ctrl.ControlSystemSimulation(intention_ctrl)
 
 # Pass inputs to the ControlSystem using Antecedent labels with Pythonic API
 # Note: if you like passing many inputs all at once, use .inputs(dict_of_data)
-intention.input['speed'] = 61
-intention.input['distance'] = 434
+intention.input['speed'] = 65
+intention.input['distance'] = 20
 
 '''
+Sample data to manually plug in
 speed =  61 office  740  boxrm  702  bedrm  434
 speed =  60 office  744  boxrm  704  bedrm  440
 speed =  41 office  757  boxrm  715  bedrm  454
+
+intention.input['speed'] = 65
+intention.input['distance'] = 20
 '''
 # Crunch the numbers
 intention.compute()

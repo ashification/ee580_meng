8e850c73a726674e85a1d83580f3b3adc9745e5a
Aisling Lee <aislinglee@gmail.com>
2022-08-18 01:42:18 +0100

    Added submit function to insert values into DB  Added Query function to query to database to get n-5 value in table
     Lastly include the updated DB file with data in it

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def on_connect(client, userdata, flags, rc, properties=None):
 
 # On publish event print value for TS
 def on_publish(client, userdata, mid, properties=None):
-    print("mid: " + str(mid))
+   # print("mid: " + str(mid))
 
 # On subscribe event print values for confirmation
 def on_subscribe(client, userdata, mid, granted_qos, properties=None):
@@ -106,19 +106,63 @@ db_cursor.execute("""CREATE TABLE tag_location (
         )""")
     db_connection.commit()
     db_connection.close()
-
-def submit():
+'''
+def submit_todb():
     # Connect to the DB
     db_connection = sqlite3.connect('user_location_tracking.db')
     # Create a Cursor
     db_cursor = db_connection.cursor()
-    db_cursor.execute("INSERT INTO tag_location VALUES (:tagid, : ")
+    db_cursor.execute("INSERT INTO tag_location VALUES (:tagid, :x1_coord, :y1_coord, :date_time)",
+                      {
+                          'tagid': "tag_1",
+                          'x1_coord': x1_coord,
+                          'y1_coord': y1_coord,
+                          'date_time': datetime.now()
+                      })
 
     db_connection.commit()
     db_connection.close()
-'''
 
+def query_fromdb():
+    # Connect to the DB
+    db_connection = sqlite3.connect('user_location_tracking.db')
+    # Create a Cursor
+    db_cursor = db_connection.cursor()
 
+    db_cursor.execute("SELECT oid FROM tag_location ORDER BY oid DESC ")
+    print(db_cursor.fetchone())
+    current_oid = db_cursor.fetchone()[0]
+    print(current_oid)
+    fifthoid = current_oid - 5
+    print(fifthoid)
+
+    db_cursor.execute("""
+                    SELECT *,oid 
+                    FROM tag_location 
+                    WHERE oid == ? 
+                    ORDER BY oid DESC 
+                    """,
+                      (fifthoid,))
+    print(db_cursor.fetchall()) #fetchmany, fetchone
+
+    db_connection.commit()
+    db_connection.close()
+
+def speed_calc():
+    #slope calc
+    return
+def distance_calc():
+   ## To Do distance of current X Y to door 1 2 and 3
+    ## Anchor co-ords
+     # 1_anchor = (60, 230)
+     # 2_anchor = (60, 590)
+     # 3_anchor = (1140, 420)
+
+     ## Door frame center co-ords
+     # office = (420 450)
+     # Box rm = (420 590)
+     # Bed rm = (770 400)
+    return
 ##############################################
 ## Define Python Simulator app
 ##############################################
@@ -133,10 +177,13 @@ bg = PhotoImage(file="Images/Floorplan_v3.png")
 tag = PhotoImage(file="Images/tag.png")
 
 #Define co-ordinates
-varwidth=800
-varheight=500
-x_coord=580
-y_coord=630
+varwidth = 800
+varheight = 500
+x_coord = 580
+y_coord = 630
+x1_coord = x_coord
+y1_coord = y_coord
+
 
 #create canvas
 canvas = Canvas(simulator_window, width=varwidth, height=varheight)
@@ -154,8 +201,6 @@ def left(event):
     y_coord = 0
     canvas.move(tag_object, x_coord, y_coord)
     # Get and Print the coordinates of the tag
-    print("Coordinates of the tag are:", canvas.coords(tag_object)[0:2])
-    client.publish("tag_topic/tag1", payload=str(canvas.coords(tag_object)[0:2]), qos=1)
     client.publish("actuator_topic/act1", payload="1", qos=1)
     client.publish("actuator_topic/act2", payload="0", qos=1)
     client.publish("actuator_topic/act3", payload="0", qos=1)
@@ -191,14 +236,19 @@ def down(event):
     canvas.move(tag_object, x_coord, y_coord)
     #Get and Print the coordinates of the tag
     print("Coordinates of the tag are:", canvas.coords(tag_object)[0:2])
-    x1_coord = int(canvas.coords(tag_object)[1])
-    y1_coord = int(canvas.coords(tag_object)[2])
+    x1_coord = int(canvas.coords(tag_object)[0])
+    y1_coord = int(canvas.coords(tag_object)[1])
+    print("x1", x1_coord, " y1 ", y1_coord,)
+    submit_todb()
+    query_fromdb()
+    '''
     time1 = datetime.now()
     sleep(1)
     time2 = datetime.now()
     timediff = time2 - time1
     timediff_convert = int(timediff.total_seconds() * 1000)  # milliseconds
     print( "x1", x1_coord, " y1 ", y1_coord, " timestamp ", timediff_convert)
+    '''
     client.publish("tag_topic/tag1", payload=str(canvas.coords(tag_object)[0:2]), qos=1)
     client.publish("actuator_topic/act1", payload="0", qos=1)
     client.publish("actuator_topic/act2", payload="0", qos=1)
@@ -208,16 +258,6 @@ simulator_window.bind("<Left>", left)
 simulator_window.bind("<Right>", right)
 simulator_window.bind("<Up>", up)
 simulator_window.bind("<Down>", down)
-
-## Anchor co-ords
-#1_anchor = (60, 230)
-#2_anchor = (60, 590)
-#3_anchor = (1140, 420)
-
-## Door frame center co-ords
-# office = (420 450)
-# Box rm = (420 590)
-# Bed rm = (770 400)
 
 client.loop_start()
 #Run Window
